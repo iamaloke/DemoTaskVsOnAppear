e7416fe053712cf9536007363657a4487a740241
Alok Kumar <alok.kumar@ovyo.com>
2025-04-30 10:44:46 +0530

    Initial Commit

---
+++ DemoTaskVsOnAppear/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoTaskVsOnAppear
+//
+//  Created by Alok Kumar on 30/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoTaskVsOnAppear/DemoTaskVsOnAppearApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTaskVsOnAppearApp.swift
+//  DemoTaskVsOnAppear
+//
+//  Created by Alok Kumar on 30/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoTaskVsOnAppearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
